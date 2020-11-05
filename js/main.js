0cd9dfa117aefcb10071e11d2acd1feb9bcf44db
akopowicz <anna.kopowicz@gmail.com>
2020-11-05 14:35:58 +0100

    wykonane zadanie

---
+++ js/main.js
@@ -0,0 +1,3 @@
+let paragraf = document.querySelector('p');
+
+paragraf.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam, numquam itaque minus distinctio, deserunt eum corporis cumque suscipit maxime consequuntur eius quaerat aliquam! Consequuntur ducimus praesentium error unde delectus.';
